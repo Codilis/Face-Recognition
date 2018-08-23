2e949109812bfec9c6305f3382a2c9fa88a2d28e
Palash Jain <palash.j22@gmail.com>
2018-08-23 20:28:42 +0530

    Code Optimised

--- Src/detector.py
+++ Src/detector.py
@@ -1,10 +1,8 @@
-import cv2,os
-import numpy as np
-from PIL import Image 
-import pickle
+import cv2
 from database import Connection
 
-conn = Connection()
+username, password = 'username', 'password'
+conn = Connection(username, password)
 conn.connect()
 data = conn.select_all()
 names_dict = {}
@@ -30,13 +28,11 @@ while True:
     ret, im =cam.read()
     gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
     faces=faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
-    #print("bye")
     for(x,y,w,h) in faces:
 ##        nbr_predicted, conf = recognizer.predict(cv2.resize(gray[y:y+h,x:x+w], (width_d, height_d)))
         nbr_predicted, conf = recognizer.predict(gray[y:y+h,x:x+w])
         cv2.rectangle(im,(x-50,y-50),(x+w+50,y+h+50),(225,0,0),2)
-        print("hello")
-        print(names_dict[str(nbr_predicted)], str(nbr_predicted)+"--"+str(conf))
+        #print(names_dict[str(nbr_predicted)], str(nbr_predicted)+"--"+str(conf))
         cv2.putText(im,names_dict[str(nbr_predicted)],
                     (x,y+h),fontFace , fontScale , fontColor) #Draw the text
     cv2.imshow('im',im)
